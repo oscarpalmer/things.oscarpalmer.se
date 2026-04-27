bcdda77d7d748af3b0ca43b474135aaf4806bd20
Oscar Palmer <opalmer@me.com>
2026-04-27 15:15:24 +0200

    nav + styling
    - banner nav for pkgs
    - nicer breadcrumbs
    - nicer search button

--- source/assets/javascript/search.ts
+++ source/assets/javascript/search.ts
@@ -77,12 +77,24 @@ const hideResults = computed(() => {
 });
 
 html`<oui-popover>
-	<button
-		oui-popover-toggle="oui-popover-toggle"
-		class="oui-button oui-button--small"
-		type="button"
-	>
-		Search
+	<button oui-popover-toggle="oui-popover-toggle" class="oui-button oui-button--tiny" type="button">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			aria-hidden="true"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+			<path d="M21 21l-6 -6" />
+		</svg>
+		<span>Search</span>
 	</button>
 	<div oui-popover-content="oui-popover-content" class="search__content">
 		<h2 class="oui-vh">Search</h2>
